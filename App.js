b83f022d68add392bd6a734234579d0806f77be4
Gabriel Santana Santos <163661965+Gabriellsa7@users.noreply.github.com>
2024-03-23 00:24:30 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,12 +1,19 @@
 import { StatusBar } from "expo-status-bar";
 import { Home } from "./src/screens/Home";
-import { View } from "react-native";
+import {  StyleSheet, View } from "react-native";
 
 export default function App() {
   return (
-    <View>
+    <View style={styles.container}>
       <Home />
       <StatusBar style="auto" />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#232F3E",
+  },
+});
